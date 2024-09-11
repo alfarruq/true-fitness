a6325a279ce2fd3394558ba51792f492c4881bee
Muhtoro <[200~a42440440@gmail.com>
2024-09-11 17:40:13 +0500

    salam

---
+++ src/compenets/home-cards/hcard.jsx
@@ -0,0 +1,13 @@
+import "./hcard.css"
+
+function Card(prop) {
+    return ( 
+        <div className="card">
+             <img src={prop.data.src} alt="" />
+             <h2 className="h2carh">{prop.data.h2}</h2>
+             <p className="pcarh">{prop.data.p}</p>
+        </div>
+     );
+}
+
+export default Card;
