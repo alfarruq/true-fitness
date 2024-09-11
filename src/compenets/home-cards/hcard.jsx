504cdf0325b89a38ba1aa77103e55e58d9e13527
Muhtoro <[200~a42440440@gmail.com>
2024-09-11 18:21:28 +0500

    added card clas name

--- src/compenets/home-cards/hcard.jsx
+++ src/compenets/home-cards/hcard.jsx
@@ -2,7 +2,7 @@ import "./hcard.css"
 
 function Card(prop) {
     return ( 
-        <div className="card">
+        <div className="cardhome">
              <img src={prop.data.src} alt="" />
              <h2 className="h2carh">{prop.data.h2}</h2>
              <p className="pcarh">{prop.data.p}</p>
